231373fb6e9cf607f0cd2c5decc540e56b7ba028
chetan929 <kumarchetan8566@gmail.com>
2025-06-26 14:01:55 +0530

    Add Google site verification HTML file and route

--- USPproject/urls.py
+++ USPproject/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('USPapp.urls')),
+    path('googled6d5880962e61c7c.html', TemplateView.as_view(
+        template_name='googled6d5880962e61c7c.html',
+        content_type='text/plain'
+    )),
 
 ]
